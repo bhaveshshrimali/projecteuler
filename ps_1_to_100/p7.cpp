fda30c6f0c7219bfb9c017264d07fa40ea3a8f83
bhaveshshrimali <bhavesh.shrimali@gmail.com>
2019-09-16 22:45:33 -0500

    add p7 -- 10001 prime

--- ps_1_to_100/p7.cpp
+++ ps_1_to_100/p7.cpp
@@ -7,6 +7,9 @@ using namespace std;
 
 bool is_prime(int n) {
     int n1 = n;
+    if (n==2){
+        return true;
+    }
     for (int i = 2; i <= n1/2; i++) {
         while (n % i == 0)
         {
@@ -22,19 +25,26 @@ bool is_prime(int n) {
 
 int main(){
 
-    // cout << is_prime(20) << "\n";
+    // cout << is_prime(2) << "\n";
 
-    // int num_prime; 
-    // cin >> num_prime; 
+    int num_prime; 
+    cin >> num_prime; 
 
-    // int j=2;
-    // int np = 0;  
-    // while (np <= num_prime){
-    //     if (is_prime(j)){
-    //         np++;
-    //     }
-    //     j++;
-    // }
-    // cout << "The " << num_prime << " -th prime is " << j << "\n";
-    // return 1;
+    int j=1;
+    int np = 0;  
+    while (np < num_prime){
+        if (is_prime(j)) {
+            np++;
+            if (np == num_prime){
+                break;
+            }
+            j++;
+        }
+        else{
+            j++;
+        }
+        // cout << "j: " << j << ", np: " << np << "\n";
+    }
+    cout << "The " << num_prime << " -th prime is " << j << "\n";
+    return 1;
 }
